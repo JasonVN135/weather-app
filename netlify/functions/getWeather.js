ddb9912e0e3ac5db87b1791dab074ff44762bb50
JasonVN135 <jasonvu135@gmail.com>
2025-03-26 13:44:30 -0400

    Updates to scripts
    Signed-off-by: JasonVN135 <jasonvu135@gmail.com>

--- netlify/functions/getWeather.js
+++ netlify/functions/getWeather.js
@@ -2,12 +2,22 @@
 exports.handler = async (event) => {
     const API_KEY = process.env.WEATHER_API_KEY; // Securely stored in Netlify
 
+    // Verify the API KEY exists
+    if (!API_KEY) {
+        console.error("API_KEY is missing");
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'API_KEY is missing' }),
+        };
+    }
+
+    console.log(API_KEY);
+
 	// Turns the passed parameter from script.js when it assigned city to the name of the city
     const city = event.queryStringParameters.city || "Raleigh"; // Default to Raleigh if no City was found
 
 	// Creates the url like normal from weatherapi using the API from Netlifty
     const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`; // URL with the city
-    
 
     console.log(url)
 	// Try to fetch the data
--- public/script.js
+++ public/script.js
@@ -1,86 +1,86 @@
 
 
-// Used to gain reference from the html file of different components
-// "temp" - Looks for a class named 'temp'
-// "time_location p" - Looks for a type p with a class name time_location
-const temperatureField = document.querySelector(".temp");
-const locationField = document.querySelector(".time_location p");
-const dateAndTimeField = document.querySelector(".time_location span");
-const conditionField = document.querySelector(".condition p");
-const conditionImageField = document.querySelector(".condition img")
-const searchField = document.querySelector(".search_area");
-const form = document.querySelector("form");
+	// Used to gain reference from the html file of different components
+	// "temp" - Looks for a class named 'temp'
+	// "time_location p" - Looks for a type p with a class name time_location
+	const temperatureField = document.querySelector(".temp");
+	const locationField = document.querySelector(".time_location p");
+	const dateAndTimeField = document.querySelector(".time_location span");
+	const conditionField = document.querySelector(".condition p");
+	const conditionImageField = document.querySelector(".condition img")
+	const searchField = document.querySelector(".search_area");
+	const form = document.querySelector("form");
 
-// When the submit action is performed in html, perform the code
-form.addEventListener('submit', searchForLocation)
+	// When the submit action is performed in html, perform the code
+	form.addEventListener('submit', searchForLocation)
 
-let target = 'Raleigh';
+	let target = 'Raleigh';
 
-const fetchResults = async (targetLocation) => {
-	// async = Asynchronous function
-	// Returns AsyncFunction object
+	const fetchResults = async (targetLocation) => {
+		// async = Asynchronous function
+		// Returns AsyncFunction object
 
-	// await = Used to pause async function execution until a promise is fulfilled
+		// await = Used to pause async function execution until a promise is fulfilled
 
-	// Fetch data from /netlify/functions/getWeather js file
-	// Passes the city as a parameter
-	const res = await fetch(`/.netlify/functions/getWeather?city=${targetLocation}`);
-	if (!res.ok) {
-		throw new Error(`Netlify function call failed: ${res.status}`);
+		// Fetch data from /netlify/functions/getWeather js file
+		// Passes the city as a parameter
+		const res = await fetch(`/.netlify/functions/getWeather?city=${targetLocation}`);
+		if (!res.ok) {
+			throw new Error(`Netlify function call failed: ${res.status}`);
+		}
+		const data = await res.json()
+
+		console.log(data)
+
+		let locationName = data.location.name
+		let locationTime = data.location.localtime
+		let locationTemp = data.current.temp_f
+		let locationCondition = data.current.condition.text
+		let locationConditionImage = data.current.condition.icon
+		populateDetails(locationTemp, locationName, locationTime, locationCondition, locationConditionImage)
 	}
-	const data = await res.json()
 
-	console.log(data)
+	function populateDetails(temp, locationName, time, condition, image) {
+		
+		let splitDate = time.split(' ')[0];
+		let splitTime = time.split(' ')[1];
+		let currentDay = getDayName(new Date(splitDate).getDay());
 
-	let locationName = data.location.name
-	let locationTime = data.location.localtime
-	let locationTemp = data.current.temp_f
-	let locationCondition = data.current.condition.text
-	let locationConditionImage = data.current.condition.icon
-	populateDetails(locationTemp, locationName, locationTime, locationCondition, locationConditionImage)
-}
+		temperatureField.innerText = temp || "N/A";
+		locationField.innerText = locationName || "Unknown Location";
+		dateAndTimeField.innerText = `${splitDate} ${currentDay} ${splitTime}` || "Unknown Time";
+		conditionField.innerText = condition || "No Condition Available";
+		
+		conditionImageField.src = image || "default-image-url.png";
+	}
 
-function populateDetails(temp, locationName, time, condition, image) {
-	
-	let splitDate = time.split(' ')[0];
-	let splitTime = time.split(' ')[1];
-	let currentDay = getDayName(new Date(splitDate).getDay());
+	function searchForLocation(e) {
+		e.preventDefault()
 
-	temperatureField.innerText = temp || "N/A";
-	locationField.innerText = locationName || "Unknown Location";
-	dateAndTimeField.innerText = `${splitDate} ${currentDay} ${splitTime}` || "Unknown Time";
-	conditionField.innerText = condition || "No Condition Available";
-	
-	conditionImageField.src = image || "default-image-url.png";
-}
+		target = searchField.value
+		searchField.value = ""
 
-function searchForLocation(e) {
-	e.preventDefault()
+		fetchResults(target)
+	}
 
-	target = searchField.value
-	searchField.value = ""
+
+	function getDayName(num) {
+		switch (num) {
+			case 0:
+				return "Sunday"
+			case 1:
+				return "Monday"
+			case 2:
+				return "Tuesday"
+			case 3:
+				return "Wednesday"
+			case 4:
+				return "Thursday"
+			case 5:
+				return "Friday"
+			case 6:
+				return "Saturday"
+		}
+	}
 
 	fetchResults(target)
-}
-
-
-function getDayName(num) {
-	switch (num) {
-		case 0:
-			return "Sunday"
-		case 1:
-			return "Monday"
-		case 2:
-			return "Tuesday"
-		case 3:
-			return "Wednesday"
-		case 4:
-			return "Thursday"
-		case 5:
-			return "Friday"
-		case 6:
-			return "Saturday"
-	}
-}
-
-fetchResults(target)
