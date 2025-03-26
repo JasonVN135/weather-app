f39f96281de86049824e6545de99a29bb1cd7280
JasonVN135 <jasonvu135@gmail.com>
2025-03-26 13:00:13 -0400

    Updated build
    Signed-off-by: JasonVN135 <jasonvu135@gmail.com>

--- netlify/functions/getWeather.js
+++ netlify/functions/getWeather.js
@@ -8,6 +8,8 @@ exports.handler = async (event) => {
 	// Creates the url like normal from weatherapi using the API from Netlifty
     const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`; // URL with the city
     
+
+    console.log(url)
 	// Try to fetch the data
     try {
 		// Fetch the response from the url
@@ -16,6 +18,7 @@ exports.handler = async (event) => {
         
 		// Get the json data from the response
         const data = await response.json();
+        console.log(data)
         
         return {
             statusCode: 200,
